3cfc91f4db74463d4b844b0c45d62404059d0a7a
Madalina Grosu <madalina92.grosu@gmail.com>
2020-12-06 16:46:27 +0100

    Day 6 Solution Part 2

--- Day6AoC/src/com/aoc/day6/Main.java
+++ Day6AoC/src/com/aoc/day6/Main.java
@@ -10,11 +10,23 @@ public class Main {
     private static int counterPart1 = 0;
     private static HashSet<Character> questionsPart1 = new HashSet<>();
 
+    private static int counterPart2 = 0;
+    private static HashSet<Character> questionsPart2 = new HashSet<>();
+    private static boolean newGroup = true;
+
     public static void main(String[] args) {
         try (Stream<String> stream = Files.lines(Paths.get("resources/input.txt"))) {
             stream.forEach(Main::analyzeLinePart1);
             counterPart1 += questionsPart1.size();
             System.out.println("Solution Part 1: " + counterPart1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (Stream<String> stream = Files.lines(Paths.get("resources/input.txt"))) {
+            stream.forEach(Main::analyzeLinePart2);
+            counterPart2 += questionsPart2.size();
+            System.out.println("Solution Part 2: " + counterPart2);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -27,6 +39,30 @@ public class Main {
         } else {
             for (int i = 0; i < line.length(); i++) {
                 questionsPart1.add(line.charAt(i));
+            }
+        }
+    }
+
+    private static void analyzeLinePart2(String line) {
+        if (line.isEmpty()) {
+            counterPart2 += questionsPart2.size();
+            questionsPart2.clear();
+            newGroup = true;
+        } else {
+            if (newGroup) {
+                for (int i = 0; i < line.length(); i++) {
+                    questionsPart2.add(line.charAt(i));
+                }
+                newGroup = false;
+            } else {
+                HashSet<Character> temp = new HashSet<>();
+                temp.addAll(questionsPart2);
+                for (Character c : temp) {
+                    if (line.indexOf(c) == -1) {
+                        questionsPart2.remove(c);
+                    }
+                }
+                temp.clear();
             }
         }
     }
